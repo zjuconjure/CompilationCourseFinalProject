401e818188fe26f1a24e5597cc7b9a4e69a8bc0b
Hanoch <NaGe720denvhai@163.com>
2019-01-14 14:43:52 +0800

    Add files via upload

---
+++ bitOperation.c
@@ -0,0 +1,14 @@
+void main() {
+	int a;
+	int b;
+	int c;
+	a = 2;
+	b = 0;
+	c = 0;
+	print(a & b);
+	print(a | b);
+	print(~a);
+	print(a << 1);
+	print(a >> 1);
+	print(b ^ c);
+}
---
+++ operationAssign.c
@@ -0,0 +1,18 @@
+void main() {
+	int a;
+	int b;
+	int c;
+	int d;
+	a = 1;
+	b = 2;
+	c = 3;
+	d = 4;
+	a += 1;
+	b -= 1;
+	c *= 3;
+	d /= 2;
+	print(a);
+	print(b);
+	print(c);
+	print(d);
+}
