fbf94f537cd12f047c5c2a98bc9a581b536dcbf5
Hanoch <NaGe720denvhai@163.com>
2019-01-14 14:44:10 +0800

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,12 @@
+void main() {
+	int grade;
+	grade = 2;
+	int result;
+	switch (grade) {
+	case 1: {result = 1; };
+	case 2: {result = 2; };
+	case 3: {result = 3; };
+	default: {result = 4; };
+	}
+	print(result);
+}
