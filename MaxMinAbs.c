0f27db3074c88adac845e0cf9d0f89a5b419361f
Hanoch <NaGe720denvhai@163.com>
2019-01-14 14:47:57 +0800

    Add files via upload

---
+++ MaxMinAbs.c
@@ -0,0 +1,17 @@
+void main() {
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+	a = 1;
+	b = 2;
+	e = -3;
+	c = Max(a, b);
+	d = Min(a, b);
+	f = Abs(e);
+	print(c);
+	print(d);
+	print(f);
+}
